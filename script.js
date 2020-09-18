a18928c14b81e452e76cfb1dc45626b234457ecd
DFuenning <dfuenning@gmail.com>
2020-09-18 10:36:04 -0400

    re-worked landing page

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
