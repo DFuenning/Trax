974cfff71c3cfcb075abb1834e5098b4ac22e413
DFuenning <dfuenning@gmail.com>
2020-11-23 16:41:05 -0500

    adding in blog page, case studies, and contact page. Updated all styling

--- script.js
+++ script.js
@@ -212,32 +212,32 @@
 
 
 
-	const counters = document.querySelectorAll('.counter');
-	const speed = 1000000000;
+	// const counters = document.querySelectorAll('.counter');
+	// const speed = 1000000000;
 
-	counters.forEach(counter => {
-		const updateCount = () => {
-			const target = +counter.getAttribute('data-target');
-			const count = +counter.innerText;
+	// counters.forEach(counter => {
+	// 	const updateCount = () => {
+	// 		const target = +counter.getAttribute('data-target');
+	// 		const count = +counter.innerText;
 
-			// Lower inc to slow and higher to slow
-			const inc = target / speed;
+	// 		// Lower inc to slow and higher to slow
+	// 		const inc = target / speed;
 
 
 
-			// Check if target is reached
-			if (count < target) {
-				// Add inc to count and output in counter
-				counter.innerText = count + inc;
-				// Call function every ms
-				setTimeout(updateCount, 100);
-			} else {
-				counter.innerText = target;
-			}
-		};
+	// 		// Check if target is reached
+	// 		if (count < target) {
+	// 			// Add inc to count and output in counter
+	// 			counter.innerText = count + inc;
+	// 			// Call function every ms
+	// 			setTimeout(updateCount, 100);
+	// 		} else {
+	// 			counter.innerText = target;
+	// 		}
+	// 	};
 
-		updateCount();
-	});
+	// 	updateCount();
+	// });
 
 
 	// Buy tickets select the ticket type on click
